8e52e560bbc4ee46cb971a8215166d070c9c340f
Ariel Izurieta <ariel_izurieta@hotmail.com>
2023-07-04 19:58:50 -0300

    exercise arithmetic operations python

---
+++ 1. Python Topics/Arithmetic operations/Exercise.py
@@ -0,0 +1,11 @@
+# Exercise | Arithmetic Operationes
+
+def exercise_arithmetic_operations():
+    number_1 = int(input("Enter 1st number: "))
+    number_2 = int(input("Enter 2nd number: "))
+    
+    print(number_1 + number_2)
+    print(number_1 - number_2)
+    print(number_1 * number_2)
+    print(round(number_1 / number_2))
+exercise_arithmetic_operations()
