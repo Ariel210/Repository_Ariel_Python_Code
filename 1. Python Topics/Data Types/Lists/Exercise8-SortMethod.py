aa65da15165e63fa4aa13a87685c4499c9a05f83
Ariel Izurieta <ariel_izurieta@hotmail.com>
2023-07-11 19:31:19 -0300

    8th exercise sorted method lists python

--- 1. Python Topics/Data Types/Lists/Exercise8-SortMethod.py
+++ 1. Python Topics/Data Types/Lists/Exercise8-SortMethod.py
@@ -1,6 +1,14 @@
+# Sort Method
 list_8 = [2,0,1,3,10,4,6,5,7,9,8]
 
 print(list_8)
 
 list_8.sort()
 print("List Ordened: ", list_8)
+
+
+# Sorted Method
+list_8_bis = [2,0,1,3,10,4,6,5,7,9,8]
+
+list_8_bis_ordened = sorted(list_8_bis)
+print("Sorted Method Applied: ", list_8_bis_ordened)
