d19e3ef4891a63ff1ded3784c9f2d7db8f42e4bf
Ariel Izurieta <ariel_izurieta@hotmail.com>
2023-07-12 19:35:05 -0300

    1st bis exercise tuples python

--- 1. Python Topics/Data Types/Tuples/Exercise1.py
+++ 1. Python Topics/Data Types/Tuples/Exercise1.py
@@ -1,4 +1,9 @@
 tuple_1 = (0,1,2,3,4,5)
+tuple_1_bis = ('ariel', 'izurieta', 29)
 
 print(tuple_1)
 print(type(tuple_1))
+
+print(tuple_1_bis)
+print(type(tuple_1_bis[0]))
+print(type(tuple_1_bis[2]))
