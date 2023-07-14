2aa6c01d2253705d5e3a2dc9bf575824b5d275be
Ariel Izurieta <ariel_izurieta@hotmail.com>
2023-07-14 20:04:22 -0300

    3rd exercise if else conditional python

---
+++ 1. Python Topics/Conditionals/Exercise3.py
@@ -0,0 +1,7 @@
+# Exercise 3 | Conditionals | Conditional if-else
+number = int(input("Enter one number: "))
+
+if number % 2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")
