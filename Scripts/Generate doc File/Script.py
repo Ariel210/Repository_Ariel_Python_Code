e511ae2bff4b5d5fa89ded64cabb9ca2c91cfd8a
Ariel Izurieta <ariel_izurieta@hotmail.com>
2023-11-06 19:51:31 -0300

    2nd exercise generate doc file python

---
+++ Scripts/Generate doc File/Script.py
@@ -0,0 +1,8 @@
+with open ('Scripts/Generate doc File/ariel.doc', 'w') as dates:
+    print("Creating doc file...")
+    dates.write("Name: Ariel \n")
+    dates.write("Surname: Izurieta")
+    dates.close()
+
+
+
