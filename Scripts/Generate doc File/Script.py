63a2f30fbec551e7b3eda2c65739fbea1066d05e
Ariel Izurieta <ariel_izurieta@hotmail.com>
2023-11-07 19:14:02 -0300

    update script generate file doc python

--- Scripts/Generate doc File/Script.py
+++ Scripts/Generate doc File/Script.py
@@ -1,8 +1,10 @@
+import os
+
 with open ('Scripts/Generate doc File/ariel.doc', 'w') as dates:
     print("Creating doc file...")
     dates.write("Name: Ariel \n")
     dates.write("Surname: Izurieta")
-    dates.close()
 
-
+print("Removing doc file...")
+os.remove('Scripts/Generate doc File/ariel.doc')
 
