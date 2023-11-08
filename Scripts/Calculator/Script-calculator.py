37369d7394f94c72b353c0c8426f6ee89349752b
Ariel Izurieta <ariel_izurieta@hotmail.com>
2023-11-08 20:08:17 -0300

    3rd adding calculator in python ariel

---
+++ Scripts/Calculator/Script-calculator.py
@@ -0,0 +1,9 @@
+def calculator_python():
+    number_1 = int(input("Enter 1st number: "))
+    number_2 = int(input("Enter 2nd number: "))
+    
+    print(number_1 + number_2)
+    print(number_1 - number_2)
+    print(number_1 * number_2)
+    print(number_1 / number_2)
+calculator_python()
