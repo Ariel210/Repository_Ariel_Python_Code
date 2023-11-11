6257592be06535f1bf1476d03f7f14b25417bdc3
Ariel Izurieta <ariel_izurieta@hotmail.com>
2023-11-11 16:06:13 -0300

    1st script for add lists in doc file by ariel

---
+++ File management/Exercise 1/script.py
@@ -0,0 +1,6 @@
+with open('File management/Exercise 1/ariel-script1.doc', 'w') as file_script_1:
+    list_1 = [0,1,2,3,4,5]
+    list_2 = [0,1,2,3,4,5]
+    file_script_1.write(str(list_1))
+    file_script_1.write(str(list_2))
+     
