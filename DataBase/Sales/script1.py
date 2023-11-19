32ab8b54b7f2f4dfbbd6a43f4beefea0b6f022f0
Ariel Izurieta <ariel_izurieta@hotmail.com>
2023-11-19 17:19:12 -0300

    update in script database with python by ariel

--- DataBase/Sales/script1.py
+++ DataBase/Sales/script1.py
@@ -5,16 +5,22 @@ def mysql_python():
     conexion = mysql.connector.connect(
         host = "localhost",
         user = str(input("Enter user name: ")),
-        password = input("Enter password: ")
+        password = input("Enter password: "),
+        database = 'ejercicio_ventas_producto'
     )
 
     
     
-    print("`\nDatabases.....")
     # Creating cursor in Python
+    print("`\nDatabases and tables.....")
     cursor = conexion.cursor()
     cursor.execute("SHOW DATABASES")
     
     for database in cursor:
         print(database)
+    
+    print("\nTables...")
+    cursor.execute("SHOW TABLES")
+    for tables in cursor:
+        print(tables)
 mysql_python()
