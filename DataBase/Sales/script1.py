ec6e9afdef5b7e5778b594b0f531ddb2da9e9712
Ariel Izurieta <ariel_izurieta@hotmail.com>
2023-11-23 21:04:09 -0300

    some updates on mysql script with python by ariel

--- DataBase/Sales/script1.py
+++ DataBase/Sales/script1.py
@@ -9,18 +9,30 @@ def mysql_python():
         database = 'ejercicio_ventas_producto'
     )
 
-    
-    
     # Creating cursor in Python
-    print("`\nDatabases and tables.....")
     cursor = conexion.cursor()
+    
+    # Database in use
+    database_use = 'ejercicio_ventas_producto'
+    cursor.execute(f"USE {database_use}")
+    
+    
+    
+    # Databases All
+    print("\nDatabases and tables.....")
     cursor.execute("SHOW DATABASES")
     
     for database in cursor:
         print(database)
     
+    # Tables all
     print("\nTables...")
     cursor.execute("SHOW TABLES")
     for tables in cursor:
-        print(tables)
+        print(tables, end=' ')
+        
+    print("\Columns Names - Product Table...")
+    cursor.execute("SELECT * FROM producto")
+    for i in cursor.column_names:
+        print(i, end=' ')
 mysql_python()
