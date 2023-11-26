f8d8a2a162d1a6263a3aa407922a9ba45cfea331
Ariel Izurieta <ariel_izurieta@hotmail.com>
2023-11-26 20:17:54 -0300

    2nd script file management with diccionary tipe by ariel

---
+++ File management/Exercise 2 - Diccionary/script-2.py
@@ -0,0 +1,4 @@
+with open ('File management/Exercise 2 - Diccionary/ariel-script1.doc', 'w') as file_script_2:
+    diccionary_ariel_dates = {'Name':'ariel','Surname':'izurieta','Age':'30'}
+    file_script_2.write(str(diccionary_ariel_dates))
+    print(str(diccionary_ariel_dates))
