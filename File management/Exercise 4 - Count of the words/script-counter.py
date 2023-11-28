9890696d57b9ac63cd8ba9b54cff884332ba7e9b
Ariel Izurieta <ariel_izurieta@hotmail.com>
2023-11-28 20:27:18 -0300

    script and updated count word in python by ariel

--- File management/Exercise 4 - Count of the words/script-counter.py
+++ File management/Exercise 4 - Count of the words/script-counter.py
@@ -1,8 +1,12 @@
-with open ('File management/Exercise 4 - Count of the words/ariel-script4.doc', 'w') as file_script_counter:
+import os
+
+with open ('File management/Exercise 4 - Count of the words/ariel-script4.txt', 'w') as file_script_counter:
     while True:
-        word = str(input("Enter word: "))
-        if len(word) <= 5:
-            file_script_counter.write(word)
-            print(word)
+        words = str(input("Enter word: "))
+        if len(words) <= 5:
+            file_script_counter.write(words + '\n' + str(len(words)))
+            print("Word: ", words)
         else:
             break
+#print("Removing txt file....")
+#os.remove('File management/Exercise 4 - Count of the words/ariel-script4.txt')
