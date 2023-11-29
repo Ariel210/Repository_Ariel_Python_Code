a992db35e15a3b6efdc6e3f152fa89e9a51c9ae5
Ariel Izurieta <ariel_izurieta@hotmail.com>
2023-11-29 20:10:51 -0300

    update on script for counter words by ariel

--- File management/Exercise 4 - Count of the words/script-counter.py
+++ File management/Exercise 4 - Count of the words/script-counter.py
@@ -1,11 +1,14 @@
 import os
 
 with open ('File management/Exercise 4 - Count of the words/ariel-script4.txt', 'w') as file_script_counter:
+    count = 0
     while True:
         words = str(input("Enter word: "))
         if len(words) <= 5:
             file_script_counter.write(words + '\n' + str(len(words)))
-            print("Word: ", words)
+            print("Word: -->", words)
+            count += 1
+            print("Count: -->", count)
         else:
             break
 #print("Removing txt file....")
