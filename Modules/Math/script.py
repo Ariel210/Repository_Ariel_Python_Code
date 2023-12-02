cd7331815dc5f8dc1e167c4f60e36d6b86950219
Ariel Izurieta <ariel_izurieta@hotmail.com>
2023-12-02 15:29:25 -0300

    script for math module in python by ariel

---
+++ Modules/Math/script.py
@@ -0,0 +1,7 @@
+import math 
+
+number = int(input("Enter Base Number: "))
+power_number = int(input("Enter Exponent number: "))
+result = math.pow(number, power_number)
+
+print(f"Result: {result}")
