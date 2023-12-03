e7a84831bd7e990834505d8956cbf5d4d19a05d7
Ariel Izurieta <ariel_izurieta@hotmail.com>
2023-12-03 19:35:39 -0300

    script for diccionary type in python by ariel

---
+++ 1. Input and Output/script-diccionary.py
@@ -0,0 +1,7 @@
+diccionary_type = {}
+
+diccionary_type.update({'Name':'Ariel', 'Surname':'Izurieta','Age':'30'})
+for key in diccionary_type.keys():
+    print(f"Keys diccionary: {key}")
+for value in diccionary_type.values():
+    print(f"Values diccionary: {value}")
