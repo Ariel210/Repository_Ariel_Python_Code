6815f47933660a6efed2bc28f37b574c50c9084e
Ariel Izurieta <ariel_izurieta@hotmail.com>
2023-12-03 20:11:22 -0300

    1st script for diccionary data type in python by ariel

---
+++ 1. Principle/DataTypes/Diccionarys/script-diccionary-1.py
@@ -0,0 +1,5 @@
+print("Diccionary Data Type...")
+diccionary_dates_ariel = {0:'Ariel',1:'Izurieta',3:'30',4:'Argentina'}
+
+print(f"Keys Method: {diccionary_dates_ariel.keys()}")
+print(f"Values Methos: {diccionary_dates_ariel.values()}")
