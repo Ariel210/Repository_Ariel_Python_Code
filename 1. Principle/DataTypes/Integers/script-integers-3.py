60f1723c507be1ac35cc793ea9acaa7d3f0c789f
Ariel Izurieta <ariel_izurieta@hotmail.com>
2023-12-09 14:09:07 -0300

    3rd script for integers data type in python by ariel

---
+++ 1. Principle/DataTypes/Integers/script-integers-3.py
@@ -0,0 +1,7 @@
+number_3 = int(input("Enter other number: "))
+print(f"Number: {number_3}")
+
+if number_3 < 0:
+    print(f"The number {number_3} is negative")
+else:
+    print(f"The number {number_3} is positive")
