68d7b605cb2580c2989fe04ca631e08e844b0eba
Ariel Izurieta <ariel_izurieta@hotmail.com>
2023-12-11 19:13:49 -0300

    update 2nd script for string data type in python by ariel

--- 1. Principle/DataTypes/String/script-string-2.py
+++ 1. Principle/DataTypes/String/script-string-2.py
@@ -1,5 +1,4 @@
 name = str(input("Enter your name: "))
 age = int(input("Enter your age: "))
 
-print("Your name is:", name + ", and your age is:", age)
-print("Your name is: {} + and your age is: {}".format(name, age))
+print("Your name and age are: ", name + '\t' + str(age))
