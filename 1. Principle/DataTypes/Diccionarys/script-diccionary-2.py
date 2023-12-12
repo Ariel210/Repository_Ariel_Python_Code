9abc2a6ada9d3d2b54170a3658a8f1f7b7752e5a
Ariel Izurieta <ariel_izurieta@hotmail.com>
2023-12-12 19:55:30 -0300

    2nd script for add in diccionary in python by ariel

---
+++ 1. Principle/DataTypes/Diccionarys/script-diccionary-2.py
@@ -0,0 +1,9 @@
+diccionary_empty_2 = {}
+
+diccionary_empty_2[0] = 'Ariel'
+diccionary_empty_2[1] = 'Izurieta'
+
+for key in diccionary_empty_2.keys():
+    print(key)
+for value in diccionary_empty_2.values():
+    print(value)
