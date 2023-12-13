fafce5cd2fd8e168b45a436e60664523a8e41cd9
Ariel Izurieta <ariel_izurieta@hotmail.com>
2023-12-13 19:38:19 -0300

    delete both files float and integer data types in pyhon by ariel

--- 1. Input and Output/script-float.py
+++
@@ -1,3 +0,0 @@
-float_number = float(input("Enter a float number: "))
-print(float_number)
-print(type(float_number))
--- 1. Input and Output/script-integer.py
+++
@@ -1,3 +0,0 @@
-integer_number = int(input("Enter a integer number: "))
-print(integer_number)
-print(type(integer_number))
