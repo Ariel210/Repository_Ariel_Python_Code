a9a8ef60cebe780a5abaf20b978a40f3d41d60ff
Ariel Izurieta <ariel_izurieta@hotmail.com>
2023-12-16 21:11:06 -0300

    4th script for integers data type in python by ariel

---
+++ 1. Principle/DataTypes/Integers/script-integers-4-sum.py
@@ -0,0 +1,7 @@
+def numbers_sum():
+    num_1= int(input("Enter 1st number: "))
+    num_2 = int(input("Enter 2nd number: "))
+    
+    return print(f"The sum is: {num_1 + num_2}")
+
+numbers_sum()
