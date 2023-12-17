892a34aab2d7a1abb49006fe15f01acffa18bae1
Ariel Izurieta <ariel_izurieta@hotmail.com>
2023-12-17 13:20:44 -0300

    update on script for sum of the numbers in python by ariel

--- 1. Principle/DataTypes/Integers/script-integers-4-sum.py
+++ 1. Principle/DataTypes/Integers/script-integers-4-sum.py
@@ -1,7 +1,6 @@
-def numbers_sum():
-    num_1= int(input("Enter 1st number: "))
-    num_2 = int(input("Enter 2nd number: "))
-    
-    return print(f"The sum is: {num_1 + num_2}")
+def numbers_sum(num_1, num_2):
+    return num_1 + num_2
 
-numbers_sum()
+num_1= int(input("Enter 1st number: "))
+num_2 = int(input("Enter 2nd number: "))
+print(f"Sum of the numbres: {numbers_sum(num_1, num_2)}")
